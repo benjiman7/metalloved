915ea1bddd4cd191eb9579cc7758a285b55a7759
benjiman7 <fortest@gmail.com>
2022-03-15 08:44:50 +0300

    Add dynamic card display
    Also added a favicon image

--- metalloved/website/models.py
+++ metalloved/website/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+# TODO: change charfield to textfield for description
+# TODO: smth with price
+
+class Card(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    # photo = models.ImageField(upload_to='static/images/photos/')
+    price = models.IntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class ContactForm(models.Model):
+    pass
--- metalloved/website/views.py
+++ metalloved/website/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Card
 
 
 def home_page(request):
-    return render(request, 'website/index.html')
+    cards = Card.objects.all()
+    return render(request, 'website/index.html', {'cards': cards})

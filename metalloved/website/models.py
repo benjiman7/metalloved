99419cb0f7eb199bddb13b822ed64501fd3b8259
benjiman7 <jarkand7@gmail.com>
2022-03-22 18:05:21 +0300

    An attempt to set up a price field for a model

--- metalloved/website/models.py
+++ metalloved/website/models.py
@@ -1,21 +1,23 @@
-from atexit import register
-from turtle import title
+from locale import currency
 from django.db import models
 
 
-# TODO: change charfield to textfield for description
-# TODO: smth with price
+# TODO: add description in admin panel for price and currency
 
 class Card(models.Model):
+    RUB = '₽'
+    NONE = 'Договорная'
+    CURRENCY_CHOICES = [(RUB, '₽'), (NONE, 'Договорная')]
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     image = models.ImageField(blank=True, default='default.svg')
-    price = models.CharField(max_length=255)
+    price = models.CharField(max_length=255, blank=True)
+    currency = models.CharField(choices=CURRENCY_CHOICES, default=RUB, max_length=10)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.title, self.price}"
+        return f"{self.title}"
 
 
 class ContactForm(models.Model):

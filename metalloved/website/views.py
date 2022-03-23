d7a641b39f0d8a42c26aaf01318bd981944a53b8
benjiman7 <jarkand7@gmail.com>
2022-03-23 22:06:19 +0300

    Some modifications in views

--- metalloved/website/views.py
+++ metalloved/website/views.py
@@ -5,10 +5,20 @@ from .models import Card
 def home_page(request):
     cards = Card.objects.all()
     prices = Card.objects.values('price')
-    for price in prices:
-        print(price)
-    #     if isinstance(price, dict):
-    #         new_card_price = f"{price} + '₽' "
-    #         print(new_card_price)
+    new_price = {
+        'new_price': ['5000', '', '7000']
+    }
+    x = new_price['new_price']
+    print(x)
+    for y in x:
+        print(y)
+    # for price in new_price:
+    #     for price1 in price:
+    #         print(price1)
+    #     new_price.update({'new_price': price})
+    #     if len(price.get('price')) < 1:
+    #         print(True)
+    #     else:
+    #         print(False)
 
-    return render(request, 'website/index.html', {'cards': cards})
+    return render(request, 'website/index.html', {'cards': cards, 'new_price': new_price})

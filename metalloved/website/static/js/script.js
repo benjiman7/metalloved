7512e52f29e1f5bc4af54f9eb5e33126ae25f00a
benjiman7 <jarkand7@gmail.com>
2022-04-04 08:48:47 +0300

    Add send bid button

---
+++ metalloved/website/static/js/script.js
@@ -0,0 +1,16 @@
+let mybutton = document.getElementById("send-bid-button");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 300 ||
+    document.documentElement.scrollTop > 300
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}

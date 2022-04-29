c07e1454a81c0c1e1ae07cffa5b395f9d76a94c9
benjiman7 <jarkand7@gmail.com>
2022-04-29 09:51:36 +0300

    Add open and close effect for service cards

--- metalloved/website/static/js/script.js
+++ metalloved/website/static/js/script.js
@@ -1,3 +1,10 @@
+const card = document.querySelector('.card');
+const cardtoggle = document.querySelector('.toggle');
+cardtoggle.onclick = function(){
+  card.classList.toggle('active')
+}
+
+
 let mybutton = document.getElementById("send-bid-button");
 
 setTimeout(()=>{
